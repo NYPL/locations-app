10eec3c3a1fa77026f9780d77e704aeb514a0687
Sean Redmond <github-smr@sneakemail.com>
2014-03-19 16:56:46 -0400

    Refactor test to be more mockable

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -4,15 +4,35 @@
 describe('Locinator controllers', function() {
 
   describe('LocationsCtrlCtrl', function(){
+    var $scope, ctrl, $timeout, locServiceMock;
 
     beforeEach(module('nypl_locations'));
 
-    it('should create "phones" model with 3 phones', inject(function($controller) {
-      var scope = {},
-          ctrl = $controller('LocationsCtrl', {$scope:scope});
+    beforeEach(function () {
+      locServiceMock = jasmine.createSpyObj('nypl_locations_service', ['all_locations']);
+      inject(function($rootScope, $controller, $q, _$timeout_) {
 
-      expect(scope.sort).toBe('name');
-    }));
+        $scope = $rootScope.$new();
+  
+        locServiceMock.all_locations.and.returnValue({
+          get: function () {return ['a', 'b', 'c', 'd']}
+        });
+      
+        $timeout = _$timeout_;
+      
+        ctrl = $controller('LocationsCtrl', {
+          $scope: $scope,
+          nypl_locations_service: locServiceMock
+        });
+      });
+    });
 
+    it('should have name as the default sort', function () {
+      expect($scope.sort).toEqual('name');
+    });
+
+    it('should load the locations during init', function () {
+      expect($scope.locations).toEqual(['a', 'b', 'c', 'd']);
+    });
   });
 });
