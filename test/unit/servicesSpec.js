e40255c268d244c65b6af3256d45af6abfa4c2d4
Rafael Hernandez <rafaelhernandez@nypl.org>
2014-03-24 15:18:52 -0400

    added a small getCoordinates test

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -2,7 +2,7 @@
 
 describe('NYPL Service Tests', function() {
 
-  describe('Utility: getDistance function', function(){
+  describe('Utility: getDistance function', function() {
 
     var nypl_coordinates_service;
 
@@ -22,7 +22,7 @@ describe('NYPL Service Tests', function() {
     });
 
     // check to see if it has the expected function
-    it('should have an getDistance() function', function () { 
+    it('should have a getDistance() function', function () { 
       expect(angular.isFunction(nypl_coordinates_service.getDistance)).toBe(true);
     });
 
@@ -31,6 +31,30 @@ describe('NYPL Service Tests', function() {
       var result = nypl_coordinates_service.getDistance(40.75298660000001, -73.9821364, 40.7619, -73.9691);
       expect(result).toBe(0.92);
       expect(result).not.toBe(null);
+    });
+  });
+
+  describe('Utility: getCoordinates function', function() {
+    var nypl_coordinates_service;
+
+    // excuted before each "it" is run.
+    beforeEach(function (){
+      
+      // load the module.
+      module('nypl_locations');
+      
+      // inject your service for testing.
+      // The _underscores_ are a convenience thing
+      // so you can have your variable name be the
+      // same as your injected service.
+      inject(function(_nypl_coordinates_service_) {
+        nypl_coordinates_service = _nypl_coordinates_service_;
+      });
+    });
+
+    // check to see if it has the expected function
+    it('should have an getCoordinates function', function () { 
+      expect(angular.isFunction(nypl_coordinates_service.getCoordinates)).toBe(true);
     });
 
   });
