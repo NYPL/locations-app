8ed16e049c1a3d1ab04723878dead308fa3c9967
Rafael Hernandez <rafaelhernandez@nypl.org>
2014-03-24 16:17:47 -0400

    added time format function, need to add as filter

--- public/scripts/services/nypl_location_service.js
+++ public/scripts/services/nypl_location_service.js
@@ -9,6 +9,14 @@ angular.module('locationService', ['ngResource']).factory('nypl_locations_servic
 		single_location: function(symbol) {
 			// return $resource('./json/jmr.json');
       return $resource('http://evening-mesa-7447-160.herokuapp.com/locations/' + symbol);
+		},
+		// Need to add as a filter
+		format_time: function(time) {
+			var components = time.split(':'),
+					hours = ((components[0] + 11) % 12) + 1;
+					minutes = components[1],
+					meridiem = components[0] > 11 ? 'pm' : 'am';
+			return hours + ":" minutes + " " + meridiem;
 		}
 	}
 });
