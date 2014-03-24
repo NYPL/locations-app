024eb742f1ba7f8fe2a6b2f65415da5cbcae3fd9
Edwin Guzman <edwin.gzmn@gmail.com>
2014-03-24 15:52:27 -0400

    Changing implementation of geocoder into two functions.

--- public/scripts/services/nypl_geocoder_service.js
+++ public/scripts/services/nypl_geocoder_service.js
@@ -2,39 +2,41 @@
 
 nypl_locations.factory('nypl_geocoder_service', ['$q', function ($q) {
   return {
-    geocoder: function (address) {
+    get_coords: function (address) {
       var defer = $q.defer();
       var coords = {};
       var geocoder = new google.maps.Geocoder();
+
+      geocoder.geocode({address: address}, function (result, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          coords.lat = result[0].geometry.location.k;
+          coords.long = result[0].geometry.location.A;
+
+          defer.resolve(coords);
+
+        } else {
+          defer.reject(new Error (status));
+        }
+      });
+
+      return defer.promise;
+    },
+    get_zipcode: function(coords) {
+      var defer = $q.defer();
+      var geocoder = new google.maps.Geocoder();
       var zipcode;
+      var latlng = new google.maps.LatLng(coords.lat, coords.lng);
 
-      if (typeof address == 'string') { 
+      geocoder.geocode({latLng: latlng}, function (result, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          var address_component = result[0].address_components;
+          zipcode =  address_component[address_component.length-1].long_name;
 
-        geocoder.geocode({address: address}, function (result, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
-            coords.lat = result[0].geometry.location.k;
-            coords.long = result[0].geometry.location.A;
-
-            defer.resolve(coords);
-
-          } else {
-            defer.reject(new Error (status));
-          }
-        });
-      } else {
-        var latlng = new google.maps.LatLng(address.lat, address.lng);
-
-        geocoder.geocode({latLng: latlng}, function (result, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
-            var address_component = result[0].address_components;
-            zipcode =  address_component[address_component.length-1].long_name;
-
-            defer.resolve(zipcode);
-          } else {
-            defer.reject(new Error (status));
-          }
-        });
-      }
+          defer.resolve(zipcode);
+        } else {
+          defer.reject(new Error (status));
+        }
+      });
 
       return defer.promise;
     }
