8186877adb9615e3ad3c1b7297c555de69afb0f5
Edwin Guzman <edwin.gzmn@gmail.com>
2014-04-04 17:02:03 -0400

    Updating map controller with promise chaining.

--- public/scripts/controllers/map.js
+++ public/scripts/controllers/map.js
@@ -1,29 +1,45 @@
 nypl_locations.controller('mapCtrl', function ($scope, $routeParams, nypl_locations_service, nypl_geocoder_service, nypl_coordinates_service, nypl_utility) {
+  var location,
+      loadLocation = function () {
+        return nypl_locations_service
+                .single_location($routeParams.symbol)
+                .then(function (data) {
+                  return data.location;
+                });
+      },
+      getUserCoords = function () {
+        return nypl_coordinates_service
+                .getCoordinates()
+                .then(function (position) {
+                  var userCoords = _.pick(position, 'latitude', 'longitude');
 
-  // Display all branches regardless of user's location
-  nypl_locations_service.single_location($routeParams.symbol).then(function (data) {
-    var location = data.location;
-    $scope.location = location;
+                  $scope.locationStart = userCoords.latitude + "," + userCoords.longitude;
+                })
+                .catch(function (error) {
+                  throw(error.message);
+                });
+      },
+      loadMapPage = function (location) {
+        $scope.location = location;
+        console.log(location);
 
-    console.log(data.location);
+        $scope.hoursToday = nypl_utility.hoursToday(location.hours);
+        $scope.locationDest = nypl_utility.getAddressString(location);
 
-    $scope.hoursToday = nypl_utility.hoursToday(location.hours);
+        var locationCoords = {
+          'lat': location.geolocation.coordinates[1],
+          'long': location.geolocation.coordinates[0]
+        };
 
-    var locationCoords = {
-      'lat': location.geolocation.coordinates[1],
-      'long': location.geolocation.coordinates[0]
-    };
+        nypl_geocoder_service.draw_map(locationCoords, 15, 'individual-map');
+        nypl_geocoder_service.draw_marker(location, 'drop', true);
+      };
 
-    nypl_geocoder_service.draw_map(locationCoords, 15, 'individual-map');
-    nypl_geocoder_service.draw_marker(location, 'drop', true);
-    
-    nypl_coordinates_service.getCoordinates().then(function (position) {
-      var userCoords = _.pick(position, 'latitude', 'longitude');
-
-      $scope.locationStart = userCoords.latitude + "," + userCoords.longitude;
-    });
-    
-    $scope.locationDest = nypl_utility.getAddressString(location);
-  });
+  loadLocation()
+    .then(loadMapPage)
+    .then(getUserCoords)
+      .catch(function(error) {
+        console.log(error)
+      });
 
 });
