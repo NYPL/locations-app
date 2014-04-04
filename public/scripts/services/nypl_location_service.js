889ea4d89db7ea037a586b97ff7a179696c854ff
Sean Redmond <github-smr@sneakemail.com>
2014-04-03 22:44:41 -0400

    Lint'd

--- public/scripts/services/nypl_location_service.js
+++ public/scripts/services/nypl_location_service.js
@@ -1,29 +1,39 @@
-'use strict';
+/*jslint indent: 4, maxlen: 80 */
+/*globals angular */
+angular.module(
+    'locationService',
+    ['ngResource']
+).factory('nypl_locations_service', function ($http, $q) {
+    'use strict';
+    var api = 'http://evening-mesa-7447-160.herokuapp.com/locations';
 
-angular.module('locationService', ['ngResource']).factory('nypl_locations_service', function($http, $q) {
+    return {
+        all_locations: function () {
+            var d = $q.defer();
 
-	return {
-		all_locations: function() {
-			var d = $q.defer();
+            $http.get(
+                api, 
+                {cache: true}
+            ).success(function (data) {
+                d.resolve(data);
+            }).error(function () {
+                d.reject();
+            });
+            return d.promise;
+        },
 
-			$http.get('http://evening-mesa-7447-160.herokuapp.com/locations', {cache: true}).success(function(data){
-				d.resolve(data);
-			}).error(function(){
-				d.reject();
-			});
-			
-			return d.promise;
-		},
-		single_location: function(symbol) {
-			var defer = $q.defer();
+        single_location: function (symbol) {
+            var defer = $q.defer();
 
-			$http.get('http://evening-mesa-7447-160.herokuapp.com/locations/' + symbol, {cache: true}).success(function(data){
-				defer.resolve(data);
-			}).error(function(){
-				defer.reject();
-			});
-
-      return defer.promise;
-		}
-	}
+            $http.get(
+                api + '/' + symbol,
+                {cache: true}
+            ).success(function (data) {
+                defer.resolve(data);
+            }).error(function () {
+                defer.reject();
+            });
+            return defer.promise;
+        }
+    };
 });
