e6b1bf7a5153a3c056373766db41f63b38c1247e
Edwin Guzman <edwin.gzmn@gmail.com>
2014-05-09 15:21:24 -0400

    Adding protractor tests for a specific division.

--- test/e2e/division/division.po.js
+++ test/e2e/division/division.po.js
@@ -1,5 +1,20 @@
 var mapPage = function () {
-  this.name = element(by.css('h1'));
+  this.name = element(by.binding('division.name'));
+
+  this.location = element(by.binding('division.location_id'));
+  this.floor = element(by.binding('division.floor'));
+  this.room = element(by.binding('division.room'));
+
+  this.division_manager = element(by.binding('division.contacts.manager'));
+
+  this.social_media = element.all(by.repeater('social in division.social_media'));
+
+  this.hoursToday = element(by.css('.hours-today'));
+  this.hours = element.all(by.repeater('hours in division.hours.regular'));
+
+  this.about_blurb = element(by.binding('division.about'));
+
+  this.blogs = element.all(by.repeater('blog in division._embedded.blogs'));
 };
 
 module.exports = new mapPage();
--- test/e2e/division/division.spec.js
+++ test/e2e/division/division.spec.js
@@ -1,16 +1,57 @@
 describe('Locations: division', function () {
   'use strict';
 
-  var mapPage = require('./division.po.js');
+  var divisionPage = require('./division.po.js');
 
+  // These tests are specific to a division
   beforeEach(function () {
-    browser.get('/#/division/photography-collection');
+    browser.get('/#/division/manuscripts-division');
     browser.waitForAngular();
   });
 
   it('should display the name', function () {
-    expect(mapPage.name.getText()).toEqual('The Miriam and Ira D. Wallach'+
-      ' Division of Art, Prints and Photographs: Photography Collection');
+    expect(divisionPage.name.getText())
+      .toEqual('Manuscripts and Archives Division');
+  });
+
+  it('should say what library it is located in', function () {
+    expect(divisionPage.location.getText())
+      .toEqual('SASB');
+  });
+
+  it('should have a floor and room number', function () {
+    // Seemds like element(by.binding()) gets the text between tags,
+    // even if there are two different {{bindings}} between those tags.
+    expect(divisionPage.floor.getText()).toEqual('Third Floor and Room #328');
+    expect(divisionPage.room.getText()).toEqual('Third Floor and Room #328');
+  });
+
+  it('should have a manager', function () {
+    expect(divisionPage.division_manager.getText())
+      .not.toBe('');
+  });
+
+  it('should display two social media icons', function () {
+    var social_media = divisionPage.social_media;
+    expect(social_media.count()).toBe(2);
+  });
+
+  it('should display hours for today', function () {
+    expect(divisionPage.hoursToday.getText()).toEqual('Open until 5:45pm');
+  });
+
+  it('should display hours for all seven days', function () {
+    var hours = divisionPage.hours;
+    expect(hours.count()).toBe(7);
+  });
+
+  it('should have a short blurb about the division', function () {
+    expect(divisionPage.about_blurb.isPresent()).toBe(true);
+  });
+
+  it('should have three blogs on the page', function () {
+    var blogs = divisionPage.blogs;
+    expect(blogs.count()).toBe(3);
   });
 
 });
