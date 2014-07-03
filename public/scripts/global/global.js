1ece48b0c760242aaced97d3c105da513e9e6c14
Rafael Hernandez <rafaelhernandez@nypl.org>
2014-07-03 15:41:45 -0400

    fixed mobile alerts positioning

--- public/scripts/global/global.js
+++ public/scripts/global/global.js
@@ -21,6 +21,7 @@ function headerScripts() {
     $('.search-options-resp').removeClass('open');
     $('#search-top').removeClass('open');
     $('#main-nav').toggleClass('open-navigation');
+    $('.sso-login').removeClass('visible');
     return false;
   });
 
@@ -31,6 +32,7 @@ function headerScripts() {
     $('#main-nav').removeClass('open-navigation');
     $('#search-block-form-input').toggleClass('open-search');
     $('#search-top').toggleClass('open');
+    $('.sso-login').removeClass('visible');
     return false;
   });
 
