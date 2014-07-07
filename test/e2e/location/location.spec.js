733fe744d40e35ddd20a91b8600df98d10bd0981
Edwin Guzman <edwin.gzmn@gmail.com>
2014-07-07 16:52:14 -0400

    Adding more e2e tests for individual circulating/research libraries and adding a mocked api call for bad events/hours/blogs

--- test/e2e/location/location.spec.js
+++ test/e2e/location/location.spec.js
@@ -9,6 +9,8 @@ describe('Locations: library', function () {
     var bad_response = {
       location: {
         "_id": "GC",
+        "_links": {},
+        "about": "",
         "access": "Fully Accessible",
         "contacts": {
             "phone": "(212) 621-0670",
@@ -23,18 +25,39 @@ describe('Locations: library', function () {
                 40.7539
             ]
         },
+        "hours": {
+          "regular": [
+            { "day": "Sun", "open": null, "close": null },
+            { "day": "Mon", "open": null, "close": null },
+            { "day": "Tue", "open": null, "close": null },
+            { "day": "Wed", "open": null, "close": null },
+            { "day": "Thu", "open": null, "close": null },
+            { "day": "Fri", "open": null, "close": null },
+            { "day": "Sat", "open": null, "close": null }
+          ],
+          "exceptions": {}
+        },
         "id": "GC",
         "image": "/sites/default/files/images/grand_central.jpg",
         "lat": null,
         "locality": "New York",
         "long": null,
-        "name": "Grand",
+        "name": "Grand Central Library",
         "postal_code": 10017,
         "region": "NY",
         "room": null,
         "slug": "grand-central",
+        "social_media": [],
         "street_address": "135 East 46th Street",
-        "type": "circulating"
+        "type": "circulating",
+        "_embedded": {
+          "services": [],
+          "events": [],
+          "exhibitions": null,
+          "blogs": [],
+          "alerts": [],
+          "divisions": []
+        }
       }
     };
 
@@ -55,13 +78,16 @@ describe('Locations: library', function () {
   describe('Circulating branch - Testing Grand Central Library',
     function () {
       beforeEach(function () {
-        // browser.addMockModule('httpBackendMock', httpBackendMock);
         browser.get('/#/grand-central');
         browser.waitForAngular();
       });
 
       it('should display the name', function () {
         expect(locationPage.name.getText()).toEqual('Grand Central Library');
+      });
+
+      it('should display the image for the library', function () {
+        expect(locationPage.image.isPresent()).toBe(true);
       });
 
       it('should have a complete address', function () {
@@ -77,8 +103,7 @@ describe('Locations: library', function () {
           .toEqual('Library Manager: Genoveve Stowell');
       });
 
-      it('should display three social media icons', function () {
-        // There are only three icons available to display
+      it('should display five social media icons', function () {
         expect(locationPage.social_media.count()).toBe(5);
       });
 
@@ -91,15 +116,27 @@ describe('Locations: library', function () {
         expect(locationPage.hours.count()).toBe(7);
       });
 
+      it('should not display any divisions', function () {
+        expect(locationPage.divisions_container.isPresent()).toBe(false);
+        expect(locationPage.divisions.count()).toBe(0);
+      });
+
       it('should display three events', function () {
+        expect(locationPage.events_container.isPresent()).toBe(true);
         expect(locationPage.events.count()).toBe(3);
       });
 
-      it('should have an about blurb', function () {
+      it('should have another image and a blurb', function () {
+        expect(locationPage.secondary_image.isPresent()).toBe(true);
         expect(locationPage.about.isPresent()).toBe(true);
       });
 
+      it('should not have a "Plan Your Visit" section', function () {
+        expect(locationPage.plan_your_visit.isPresent()).toBe(false);
+      });
+
       it('should display three blogs', function () {
+        expect(locationPage.blogs_container.isPresent()).toBe(true);
         expect(locationPage.blogs.count()).toBe(3);
       });
 
@@ -182,23 +219,31 @@ describe('Locations: library', function () {
       expect(locationPage.hoursToday.getText()).not.toEqual('');
     });
 
+    it('should display five social media icons', function () {
+      expect(locationPage.social_media.count()).toBe(5);
+    });
+
     it('should display hours for all seven days', function () {
       expect(locationPage.hours.count()).toBe(7);
     });
 
     it('should display 5 divisions', function () {
+      expect(locationPage.divisions_container.isPresent()).toBe(true);
       expect(locationPage.divisions.count()).toBe(5);
     });
 
     it('should display three events', function () {
+      expect(locationPage.events_container.isPresent()).toBe(true);
       expect(locationPage.events.count()).toBe(3);
     });
 
-    it('should have an about blurb', function () {
+    it('should have a "Plan your visit" and about blurb section', function () {
+      expect(locationPage.plan_your_visit.isPresent()).toBe(true);
       expect(locationPage.about.isPresent()).toBe(true);
     });
 
     it('should not display blogs', function () {
+      expect(locationPage.blogs_container.isPresent()).toBe(false);
       expect(locationPage.blogs.count()).toBe(0);
     });
 
@@ -256,4 +301,40 @@ describe('Locations: library', function () {
       });
     });
   });
+
+  describe('Circulating branch - Bad API call', function () {
+   beforeEach(function () {
+      browser.addMockModule('httpBackendMock', httpBackendMock);
+      browser.get('/#/grand-central');
+      browser.waitForAngular();
+    });
+
+    describe('Bad hours', function () {
+      it('should say that the library is closed every day', function () {
+        expect(locationPage.hoursToday.getText()).toEqual('Closed today');
+
+        var i = 0;
+        var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        locationPage.hours.each(function (h) { 
+          expect(h.getText()).toEqual(days[i] + ' Closed');
+          i++;
+        });
+      });
+    });
+
+    describe('No events in the API', function () {
+      it('should not generate the Events block on the page', function () {
+        expect(locationPage.events_container.isPresent()).toBe(false);
+        expect(locationPage.events.count()).toBe(0);
+      });
+    });
+
+    describe('No blogs in the API', function () {
+      it('should not genereate the Blogs block on the page', function () {
+        expect(locationPage.blogs_container.isPresent()).toBe(false);
+        expect(locationPage.blogs.count()).toBe(0);
+      });
+    });
+  });
+
 });
