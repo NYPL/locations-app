17372c77efced68768b603f096b1331f86237eba
Sean Redmond <github-smr@sneakemail.com>
2014-09-02 16:43:55 -0400

    Separate handler for _escaped_fragment_
    Provide a separate handler for URLs with the _escaped_fragment_
    parameter. This method doesn't work with the sinatra-static gem so it
    was removed and a handler created specifically for the root URL.

--- locinator.rb
+++ locinator.rb
@@ -1,10 +1,24 @@
 require 'sinatra/base'
-require 'sinatra-index'
 
 class Locinator < Sinatra::Base
-  register Sinatra::Index
-  use_static_index 'index.html'
-  # ... app code here ...
+
+  # Method cribbed from http://blog.alexmaccaw.com/seo-in-js-web-apps
+  helpers do
+    set :spider do |enabled|
+      condition do
+        params.has_key?('_escaped_fragment_')
+      end
+    end
+  end
+
+  get '/', :spider => true do
+    "Escaped Fragment"
+  end
+
+  get '/' do
+    File.read(File.join('public', 'index.html'))
+  end
+  
 
   # start the server if ruby file executed directly
   run! if app_file == $0
