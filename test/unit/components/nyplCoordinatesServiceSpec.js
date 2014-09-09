8d25f3db3161ec0a1a20ba574754a37b91a6b88d
Rafael Hernandez <rafaelhernandez@nypl.org>
2014-09-09 16:05:58 -0400

    Updated coordinates geolocation test to pass for window object

--- test/unit/components/nyplCoordinatesServiceSpec.js
+++ test/unit/components/nyplCoordinatesServiceSpec.js
@@ -16,11 +16,17 @@ describe('NYPL coordinateService Module', function () {
    * browser's current location, and computes coordinate distance.
    */
   describe('nyplCoordinatesService', function () {
-    var nyplCoordinatesService;
+    var nyplCoordinatesService, $window;
 
     beforeEach(function () {
       // load the module.
       module('coordinateService');
+
+      $window = {navigator: { geolocation: jasmine.createSpy()} };
+
+      module(function($provide) {
+        $provide.value('$window', $window);
+      });
 
       // inject your service for testing.
       // The _underscores_ are a convenience thing
@@ -38,6 +44,7 @@ describe('NYPL coordinateService Module', function () {
      *   browser, false otherwise.
      */
     describe('nyplCoordinatesService.geolocationAvailable()', function () {
+
       it('should have a geolocationAvailable function', function () {
         expect(angular.isFunction(nyplCoordinatesService.geolocationAvailable))
           .toBe(true);
@@ -46,17 +53,12 @@ describe('NYPL coordinateService Module', function () {
 
       it('should return false due to old browser', function () {
         // Old browsers don't have the navigator api
-        window.navigator = false;
-        window.navigator.geolocation = false;
-
+        $window.navigator = false;
+        $window.navigator.geolocation = false;
         expect(nyplCoordinatesService.geolocationAvailable()).toBe(false);
       });
 
       it('should return true for modern browsers', function () {
-        // Modern browsers have the navigator api
-        window.navigator = true;
-        window.navigator.geolocation = true;
-
         expect(nyplCoordinatesService.geolocationAvailable()).toBe(true);
       });
     }); /* End nyplCoordinatesService.geolocationAvailable() */
@@ -72,9 +74,9 @@ describe('NYPL coordinateService Module', function () {
 
       beforeEach(inject(function (_$rootScope_) {
         $rootScope = _$rootScope_;
-        window.navigator = jasmine.createSpy('navigator');
+        $window.navigator = jasmine.createSpy('navigator');
         geolocationMock =
-          window.navigator.geolocation = jasmine.createSpy('geolocation');
+          $window.navigator.geolocation = jasmine.createSpy('geolocation');
       }));
 
       // check to see if it has the expected function
