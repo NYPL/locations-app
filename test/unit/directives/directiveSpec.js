eac43d73eb146adf867766e20c51cc5428ad6beb
Edwin Guzman <edwin.gzmn@gmail.com>
2014-09-22 15:27:34 -0400

    Moving breadcrumbs directive test into it's own file since it is its own component.

--- test/unit/directives/directiveSpec.js
+++ test/unit/directives/directiveSpec.js
@@ -196,46 +196,6 @@ describe('NYPL Directive Unit Tests', function () {
   }); /* End todayshours */
 
   /*
-   * <nyplbreadcrumbs></nyplbreadcrumbs>
-   */
-  describe('Directive: nyplbreadcrumbs', function () {
-    var nyplbreadcrumbs, html;
-
-    beforeEach(inject(function () {
-      html = '<nypl-breadcrumbs crumb-name="data.crumbName"></nypl-breadcrumbs>';
-      nyplbreadcrumbs = createDirective(html);
-    }));
-
-    it('should create an unordered list with class breadcrumb', function () {
-      var crumbList = nyplbreadcrumbs.find('ul');
-      expect(crumbList.attr('class')).toContain('breadcrumb');
-    });
-
-    it('should contain attribute "crumb-name"', function () {
-      expect(nyplbreadcrumbs.attr('crumb-name')).toBeTruthy();
-    });
-
-    it('should contain attribute "crumb-name" value to be "data.crumbName"', function () {
-      expect(nyplbreadcrumbs.attr('crumb-name')).toBe('data.crumbName');
-    });
-
-    it('should create an empty breadcrumbs scope array element', function () {
-      var isoScope = nyplbreadcrumbs.isolateScope();
-      expect(isoScope.breadcrumbs.length).toBeLessThan(1);
-    });
-
-    it('once a Crumb is inserted, it should add elements to breadcrumbs array', function () {
-      var isoScope = nyplbreadcrumbs.isolateScope();
-
-      isoScope.breadcrumbs.push({
-        displayName: 'Amenities',
-        route: 'amenities'
-      });
-      expect(isoScope.breadcrumbs.length).toBeGreaterThan(0);
-    });
-  });
-
-  /*
    * <emailusbutton link="" />
    *   Generates a link to email a librarian.
    */
