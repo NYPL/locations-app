bb6fdfffa5608ff32bd1ec3ca38ef15ab8d5c548
Edwin Guzman <edwin.gzmn@gmail.com>
2014-10-21 11:58:54 -0400

    Adding the feedback component back into the Locinator app to test on AWS.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,7 +7,7 @@ var nypl_locations = angular.module('nypl_locations', [
     'ngAnimate',
     'locationService',
     'coordinateService',
-    // 'nyplFeedback',
+    'nyplFeedback',
     'nyplSearch',
     'nyplSSO',
     'nyplNavigation',
