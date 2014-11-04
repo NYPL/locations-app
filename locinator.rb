f98901abebe062fbcb3dbe52c14e4015eedae056
Edwin Guzman <edwin.gzmn@gmail.com>
2014-11-04 11:45:44 -0500

    Adding backend check to output the correct base href locally and on different envs. Testing.

--- locinator.rb
+++ locinator.rb
@@ -18,6 +18,8 @@ class Locinator < Sinatra::Base
     set :featured_amenities, configs["featured_amenities"]
     set :research_order, configs["research_order"]
     set :fundraising, configs["fundraising"]
+    set :root, 'locations/'
+    set :views, 'views'
   end
 
   before do
