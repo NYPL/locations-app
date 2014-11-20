00f1ff2cefc39ca51e01610ecd6c5c7370598cd3
Edwin Guzman <edwin.gzmn@gmail.com>
2014-11-20 12:25:23 -0500

    Small update to router unit test spec.

--- test/unit/appRoutesSpec.js
+++ test/unit/appRoutesSpec.js
@@ -118,8 +118,7 @@ describe('Locinator State Routing', function () {
         
         testStateConfig(stateConfig, expectedConfig);
         expect($injector.invoke(stateConfig.resolve.config)).toEqual(configObj);
-        // expect($injector.invoke(stateConfig.resolve.division).then)
-        //   .toBeDefined();
+        // expect($injector.invoke(stateConfig.resolve.division).then).toBeDefined();
       });
 
       it('should return amenities state', function () {
@@ -289,6 +288,7 @@ describe('Locinator State Routing', function () {
         
         testStateConfig(stateConfig, expectedConfig);
         expect($injector.invoke(stateConfig.resolve.config)).toEqual(configObj);
+        expect($injector.invoke(stateConfig.resolve.data)).toBeDefined();
       });
 
       it('should return the division state', function () {
@@ -304,6 +304,7 @@ describe('Locinator State Routing', function () {
         
         testStateConfig(stateConfig, expectedConfig);
         expect($injector.invoke(stateConfig.resolve.config)).toEqual(configObj);
+        // expect($injector.invoke(stateConfig.resolve.data)).toBeDefined();
       });
 
       it('should return the locations state', function () {
@@ -319,6 +320,7 @@ describe('Locinator State Routing', function () {
         
         testStateConfig(stateConfig, expectedConfig);
         expect($injector.invoke(stateConfig.resolve.config)).toEqual(configObj);
+        // expect($injector.invoke(stateConfig.resolve.data)).toBeDefined();
       });
     });
 
