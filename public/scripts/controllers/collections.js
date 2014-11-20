27199c2745b686168923f61bc843abcf79813223
Edwin Guzman <edwin.gzmn@gmail.com>
2014-11-20 17:44:29 -0500

    LOC-457 Using id for term when selecting subterms.

--- public/scripts/controllers/collections.js
+++ public/scripts/controllers/collections.js
@@ -1,81 +1,81 @@
-/*jslint indent: 4, maxlen: 80, nomen: true */
+/*jslint indent: 2, maxlen: 80, nomen: true */
 /*globals nypl_locations, _, angular, jQuery, $location, $ */
 
 (function () {
+  'use strict';
+
+  function CollectionsCtrl(
+    $scope,
+    $rootScope,
+    config,
+    divisions,
+    terms,
+    nyplLocationsService,
+    nyplUtility
+  ) {
     'use strict';
+    $rootScope.title = "Research Collections";
+    $scope.terms = terms;
+    $scope.divisions = divisions;
+    var getHoursToday = function(obj) {
+      _.each(obj, function (elem) {
+        if (elem.hours) {
+          elem.hoursToday = nyplUtility.hoursToday(elem.hours);
+        }
+      });
+    };
 
-    function CollectionsCtrl(
-        $scope,
-        $rootScope,
-        config,
-        divisions,
-        terms,
-        nyplLocationsService,
-        nyplUtility
-    ) {
-        'use strict';
-        $rootScope.title = "Research Collections";
-        $scope.terms = terms;
-        $scope.divisions = divisions;
-        var getHoursToday = function(obj) {
-            _.each(obj, function (elem) {
-                if (elem.hours) {
-                    elem.hoursToday = nyplUtility.hoursToday(elem.hours);
-                }
+    $scope.setSubterms = function (index, term) {
+      var subterms;
+      if (term.id == 42) {
+        subterms = _.chain(terms[index].terms)
+          .pluck('terms')
+          .flatten(true)
+          .unique()
+          .value();
+      } else {
+        subterms = terms[index].terms;
+      }
+
+      $scope.termIndex = index;
+      $scope.subterms = subterms;
+    };
+
+    $scope.filterDivisions = function (name) {
+      var updatedDivisions = [],
+          i;
+
+      for (i = 0; i < divisions.length; i++) {
+        if (divisions[i].terms) {
+            console.log(divisions[i].name);
+            _.each(divisions[i].terms, function (term) {
+              console.log(term.terms);
+
+              // if (_.contains(term.terms, {name: name})) {
+              //     console.log('test');
+              // }
+              var test = _.find(term.terms, function (term) {
+                // console.log(term);
+                return term.name = name;
+              });
+
+              console.log(test);
             });
-        };
+            // console.log(divisions[i].terms[$scope.termIndex].terms)
+            // if (_.has(divisions[i].terms[$scope.termIndex].terms, name)) {
+            //     console.log('test');
+            // }
+        }
+      }
 
-        $scope.setSubterms = function (index, name) {
-            var subterms;
-            if (name == 'Subjects') {
-                subterms = _.chain(terms[index].terms)
-                    .pluck('terms')
-                    .flatten(true)
-                    .unique()
-                    .value();
-            } else {
-                subterms = terms[index].terms;
-            }
+      $scope.divisions = updatedDivisions;
+    };
 
-            $scope.termIndex = index;
-            $scope.subterms = subterms;
-        };
+    // Assign Today's hours
+    getHoursToday($scope.divisions);
+  }
 
-        $scope.filterDivisions = function (name) {
-            var updatedDivisions = [],
-                i = 0;
-
-            for (i; i < divisions.length; i++) {
-                if (divisions[i].terms) {
-                    console.log(divisions[i].name);
-                    _.each(divisions[i].terms, function (term) {
-                        console.log(term.terms);
-
-                        // if (_.contains(term.terms, {name: name})) {
-                        //     console.log('test');
-                        // }
-                        var test = _.find(term.terms, function (term) {
-                            // console.log(term);
-                            return term.name = name;
-                        });
-
-                        console.log(test);
-                    });
-                    // console.log(divisions[i].terms[$scope.termIndex].terms)
-                    // if (_.has(divisions[i].terms[$scope.termIndex].terms, name)) {
-                    //     console.log('test');
-                    // }
-                }
-            }
-
-            $scope.divisions = updatedDivisions;
-        };
-
-        // Assign Today's hours
-        getHoursToday($scope.divisions);
-    }
-
-    angular
-        .module('nypl_locations')
-        .controller('CollectionsCtrl', CollectionsCtrl);
+  angular
+    .module('nypl_locations')
+    .controller('CollectionsCtrl', CollectionsCtrl);
 })();
