ee0ef64c1aa47e0474e569ad2d64842a48a20ada
Edwin Guzman <edwin.gzmn@gmail.com>
2015-01-14 17:13:20 -0500

    Fixing :triangular_ruler: E2E tests that mock data. Needed to update the API url to use a version.

--- test/e2e/location/research.spec.js
+++ test/e2e/location/research.spec.js
@@ -8,11 +8,11 @@ describe('Research branch page', function () {
   var locationPage = require('./location.po.js'),
     APIresponse = require('../APImocks/research.js'),
     httpBackendMock = function (response) {
-      var API_URL = 'http://dev.locations.api.nypl.org';
+      var API_URL = 'http://dev.locations.api.nypl.org/api/v0.5';
 
       angular.module('httpBackendMock', ['ngMockE2E'])
         .run(function ($httpBackend) {
-          $httpBackend.whenGET('languages/en.json').passThrough();
+          // $httpBackend.whenGET('languages/en.json').passThrough();
 
           $httpBackend
             .whenJSONP(API_URL + '/locations/schomburg?callback=JSON_CALLBACK')
