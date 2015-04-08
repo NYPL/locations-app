803bef3bad0d7ddf42e9e023ca8a0f28ee106418
Edwin Guzman <edwin.gzmn@gmail.com>
2015-04-08 16:31:59 -0400

    LOC-676 Sorting in the ng-repeat explicitly by the parameter that is passed - for viewOnMap function as well.

--- public/scripts/controllers/locations.js
+++ public/scripts/controllers/locations.js
@@ -191,7 +191,7 @@
                     nyplSearch.setSearchValue('locations', $scope.locations);
                     // Don't sort by distance or the matched results
                     // will not display first.
-                    sortListBy('distance');
+                    sortListBy(sortByFilter);
                 },
 
             scrollListTop = function () {
