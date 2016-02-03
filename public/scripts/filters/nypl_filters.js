deca0f7fe5b0f6437b0c99c41148c1deddc72d7d
Kang Peng <kangtingpeng@nypl.org>
2016-02-03 17:21:42 -0500

    Updated time format for open hours for divisions.

--- public/scripts/filters/nypl_filters.js
+++ public/scripts/filters/nypl_filters.js
@@ -26,10 +26,10 @@
         function clockTime(time) {
             var components = time.split(':'),
                 hours = ((parseInt(components[0], 10) + 11) % 12 + 1),
-                minutes = components[1],
+                minutes = (components[1] == '00') ? '' : ':' + components[1],
                 meridiem = components[0] >= 12 ? ' PM' : ' AM';
 
-            return hours + ":" + minutes + meridiem;
+            return hours + minutes + meridiem;
         }
 
         function closingHoursDisplay(hours, alerts) {
